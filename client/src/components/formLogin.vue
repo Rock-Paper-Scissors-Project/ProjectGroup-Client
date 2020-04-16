1d1b9a7c260ead2af2614e577a2a3d9a3eb177d9
iguntop <Guntoro>
2020-04-16 16:58:10 +0700

    comunication 2 ways success

--- client/src/components/formLogin.vue
+++ client/src/components/formLogin.vue
@@ -2,16 +2,40 @@
   <div>
     <form>
         <p><b>Username:</b></p>
-        <input type="text" placeholder="Your user name here">
-        <input type="submit" value="Log in">
+        <input type="text" placeholder="Your user name here" v-model="username">
+        <input type="submit" value="Log in" @click.prevent="addmember()">
     </form>
   </div>
 </template>
 
 <script>
+import io from 'socket.io-client'
 export default {
-  name: 'loginForm'
+  name: 'loginForm',
+  data () {
+    return {
+      username: ''
+    }
+  },
+  methods: {
+    addmember () {
+      const user = this.username
+      console.log('user dikirim ke server', user)
+      var socket = io.connect('http://localhost:3000')
+      socket.emit('add user', { user })
+      // socket.on('add user', function (user) {
+      //   console.log('add member')
+      // })
+    }
+  },
+  created () {
+    console.log('Created')
+    io.connect('http://localhost:3000').on('get user', (data) => {
+      console.log('user dari server', data)
+    })
+  }
 }
+
 </script>
 
 <style>
